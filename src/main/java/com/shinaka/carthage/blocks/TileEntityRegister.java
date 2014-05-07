bc9d94716f0bbddaafc81a2ff9b6b1749919d686
Shinaka <shinaka@shinaka.com>
2014-05-07 00:43:27 -0400

    Finished Register TileEntity data (inventory) persisting between saves (nbt) all before succuming to Ambien!

--- src/main/java/com/shinaka/carthage/blocks/TileEntityRegister.java
+++ src/main/java/com/shinaka/carthage/blocks/TileEntityRegister.java
@@ -3,11 +3,10 @@ package com.shinaka.carthage.blocks;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 
-/**
- * Created by James on 5/3/2014.
- */
 public class TileEntityRegister extends TileEntity implements IInventory
 {
     private ItemStack slot1;
@@ -15,12 +14,97 @@ public class TileEntityRegister extends TileEntity implements IInventory
     private ItemStack slot3;
     private ItemStack slot4;
     private ItemStack paperStack;
-    private ItemStack[] registerSlots;
 
     public TileEntityRegister()
     {
 
     }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt)
+    {
+        super.readFromNBT(nbt);
+
+        NBTTagList tagList = nbt.getTagList("Inventory", 10);
+        for (int i = 0; i < tagList.tagCount(); i++)
+        {
+            NBTTagCompound tag = tagList.getCompoundTagAt(i);
+            byte slot = tag.getByte("Slot");
+            if (slot >= 0 && slot < getSizeInventory())
+            {
+                switch(slot)
+                {
+                    case 0:
+                        slot1 = ItemStack.loadItemStackFromNBT(tag);
+                        break;
+                    case 1:
+                        slot2 = ItemStack.loadItemStackFromNBT(tag);
+                        break;
+                    case 2:
+                        slot3 = ItemStack.loadItemStackFromNBT(tag);
+                        break;
+                    case 3:
+                        slot4 = ItemStack.loadItemStackFromNBT(tag);
+                        break;
+                    case 4:
+                        paperStack = ItemStack.loadItemStackFromNBT(tag);
+                        break;
+                }
+            }
+        }
+    }
+
+    public NBTTagCompound writeSlotItemToTag(int slot)
+    {
+        NBTTagCompound tag = new NBTTagCompound();
+        ItemStack theSlot = null;
+        switch(slot)
+        {
+            case 0:
+                theSlot = slot1;
+                break;
+            case 1:
+                theSlot = slot2;
+                break;
+            case 2:
+                theSlot = slot3;
+                break;
+            case 3:
+                theSlot = slot4;
+                break;
+            case 4:
+                theSlot = paperStack;
+                break;
+        }
+
+        if(theSlot == null)
+            return null;
+
+        tag.setByte("Slot", (byte) slot);
+        theSlot.writeToNBT(tag);
+        return tag;
+
+    }
+    @Override
+    public void writeToNBT(NBTTagCompound nbt)
+    {
+        super.writeToNBT(nbt);
+
+        NBTTagList itemList = new NBTTagList();
+        if(slot1 != null)
+            itemList.appendTag(writeSlotItemToTag(0));
+        if(slot2 != null)
+            itemList.appendTag(writeSlotItemToTag(1));
+        if(slot3 != null)
+            itemList.appendTag(writeSlotItemToTag(2));
+        if(slot3 != null)
+            itemList.appendTag(writeSlotItemToTag(3));
+        if(paperStack != null)
+            itemList.appendTag(writeSlotItemToTag(4));
+
+        nbt.setTag("Inventory", itemList);
+    }
+
     @Override
     public int getSizeInventory() {
         return 5;
