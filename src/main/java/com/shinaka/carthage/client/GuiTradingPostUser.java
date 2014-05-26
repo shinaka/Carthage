140d29500dd2da9bfaf7b9b80a686246e2f6b138
Shinaka <shinaka@shinaka.com>
2014-05-26 16:36:51 -0400

    Visual side of the TP Non-Owner GUI

--- src/main/java/com/shinaka/carthage/client/GuiTradingPostUser.java
+++ src/main/java/com/shinaka/carthage/client/GuiTradingPostUser.java
@@ -2,6 +2,7 @@ package com.shinaka.carthage.client;
 
 import com.shinaka.carthage.ContainerTradingPostUser;
 import com.shinaka.carthage.blocks.TileEntityTradingPost;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
@@ -38,5 +39,34 @@ public class GuiTradingPostUser extends CustomGuiContainer
         this.mc.renderEngine.bindTexture(bg);
 
         this.drawTexturedModalRect(getGuiLeft(), getGuiTop(), 0, 0, 256, 256);
+
+        //Draw our input slot
+        this.drawItemSlot(16, 16, 12, 17);
+
+        //Draw available item slots
+        this.drawItemSlot(16, 16, 135, 8);
+        this.drawItemSlot(16, 16, 135, 26);
+        this.drawItemSlot(16, 16, 135, 44);
+        this.drawItemSlot(16, 16, 135, 62);
+
+        //Out Slot
+        this.drawItemSlot(16, 16, 12, 62);
+
+        FontRenderer fontRenderer = this.mc.fontRenderer;
+
+        fontRenderer.drawString("Sell", getGuiLeft() + 11, getGuiTop() + 7, 4210752);
+        fontRenderer.drawString("Buy", getGuiLeft() + 11, getGuiTop() + 52, 4210752);
+
+        fontRenderer.drawString("Available Credits", getGuiLeft() + 42, getGuiTop() + 27, 4210752);
+
+        //Player's Credit Count
+        fontRenderer.drawString("100", getGuiLeft() + 74, getGuiTop() + 42, 4210752);
+
+        //Credit Text
+        fontRenderer.drawString("100", getGuiLeft() + 153, getGuiTop() + 12, 4210752);
+        fontRenderer.drawString("100", getGuiLeft() + 153, getGuiTop() + 30, 4210752);
+        fontRenderer.drawString("100", getGuiLeft() + 153, getGuiTop() + 48, 4210752);
+        fontRenderer.drawString("100", getGuiLeft() + 153, getGuiTop() + 66, 4210752);
+
     }
 }
