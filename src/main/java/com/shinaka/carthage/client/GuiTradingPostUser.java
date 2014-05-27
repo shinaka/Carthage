29ee278973393543a1f8c119cc0bf5e72c202db4
Shinaka <shinaka@shinaka.com>
2014-05-26 20:55:50 -0400

    Fixing rendering issue caused by Text rendering before textured model rects

--- src/main/java/com/shinaka/carthage/client/GuiTradingPostUser.java
+++ src/main/java/com/shinaka/carthage/client/GuiTradingPostUser.java
@@ -50,11 +50,7 @@ public class GuiTradingPostUser extends CustomGuiContainer
 
         this.drawTexturedModalRect(getGuiLeft(), getGuiTop(), 0, 0, 256, 256);
 
-        //Draw available item slots
-        this.drawItemSlot(16, 16, 135, 8);
-        this.drawItemSlot(16, 16, 135, 26);
-        this.drawItemSlot(16, 16, 135, 44);
-        this.drawItemSlot(16, 16, 135, 62);
+
 
         int itemHeight = 8;
         final int itemSpace = 18;
@@ -63,20 +59,32 @@ public class GuiTradingPostUser extends CustomGuiContainer
             IIcon icon = item.getIconIndex();
             ResourceLocation resourceLocation = texMgr.getResourceLocation(item.getItem().getSpriteNumber());
             Minecraft.getMinecraft().renderEngine.bindTexture(resourceLocation);
+
             this.drawTexturedModelRectFromIcon(getGuiLeft() + 135, getGuiTop() + itemHeight, icon, 16, 16);
-            fontRenderer.drawString(Integer.toString(tePost.GetItemLedgerCost(item)), getGuiLeft() + 153, getGuiTop() + itemHeight + 4, 4210752);
+
             itemHeight = itemHeight + itemSpace;
         }
 
+        //We can't draw text until after we've rendered these (or else the textures rendered after get dimmed
+        //So, we have to be a little sloppy.
+        itemHeight = 8;
+        for(ItemStack item : ledgerItems)
+        {
+            fontRenderer.drawString(Integer.toString(tePost.GetItemLedgerCost(item)), getGuiLeft() + 153, getGuiTop() + itemHeight + 4, 4210752);
+            itemHeight = itemHeight + itemSpace;
+        }
+        /*
+        //Draw available item slots
+        this.drawItemSlot(16, 16, 135, 8);
+        this.drawItemSlot(16, 16, 135, 26);
+        this.drawItemSlot(16, 16, 135, 44);
+        this.drawItemSlot(16, 16, 135, 62);
+        */
         //Draw our input slot
         this.drawItemSlot(16, 16, 12, 17);
 
-
-
         //Out Slot
         this.drawItemSlot(16, 16, 12, 62);
-
-
 
         fontRenderer.drawString("Sell", getGuiLeft() + 11, getGuiTop() + 7, 4210752);
         fontRenderer.drawString("Buy", getGuiLeft() + 11, getGuiTop() + 52, 4210752);
@@ -85,14 +93,5 @@ public class GuiTradingPostUser extends CustomGuiContainer
 
         //Player's Credit Count
         fontRenderer.drawString(Integer.toString(tePost.GetCreditsForUser(player.getDisplayName())), getGuiLeft() + 74, getGuiTop() + 42, 4210752);
-
-        /*
-        //Credit Text
-        fontRenderer.drawString("100", getGuiLeft() + 153, getGuiTop() + 12, 4210752);
-        fontRenderer.drawString("100", getGuiLeft() + 153, getGuiTop() + 30, 4210752);
-        fontRenderer.drawString("100", getGuiLeft() + 153, getGuiTop() + 48, 4210752);
-        fontRenderer.drawString("100", getGuiLeft() + 153, getGuiTop() + 66, 4210752);
-        */
-
     }
 }
